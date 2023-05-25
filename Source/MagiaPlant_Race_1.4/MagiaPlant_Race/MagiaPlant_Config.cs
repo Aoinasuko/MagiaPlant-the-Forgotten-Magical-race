f58bdc9dfb6539a04aa01db02d191a67f4e35310
Aoi_Nasuko <aoinasuko.nasunasu@gmail.com>
2023-05-25 21:50:13 +0900

    0525Fix

--- Source/MagiaPlant_Race_1.4/MagiaPlant_Race/MagiaPlant_Config.cs
+++ Source/MagiaPlant_Race_1.4/MagiaPlant_Race/MagiaPlant_Config.cs
@@ -29,7 +29,7 @@ namespace MagiaPlant_Race
 	public class MagiaPlant_Config : ModSettings
 	{
 		// バージョン
-		public static int ver = 110;
+		public static int ver = 200;
 
 		// アップデートバージョン
 		public static int Updatever = 0;
